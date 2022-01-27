fc17384891c20c532f42ddc29bcd92e2b54abaf7
Lisa Sun <sunlisa@gmail.com>
2022-01-26 22:57:43 -0600

    question class take set as parameter

--- lib/main.dart
+++ lib/main.dart
@@ -39,9 +39,9 @@ class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
   List<Question> questions = [
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
   int questionNumber = 0;
