c8202016052ac546b49034d5cf5e254dc24e072e
Lisa Sun <sunlisa@gmail.com>
2022-01-26 22:58:05 -0600

    question class take set as parameter

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,9 @@ class _QuizPageState extends State<QuizPage> {
 
   List<Question> questions = [
     Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
     Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
